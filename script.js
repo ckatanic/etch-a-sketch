e1fe7783b85ede574641edca607e0cd8696ef4b3
Chris <ckatanic@gmail.com>
2023-08-04 13:06:02 -0700

    add color change when mouse enters with mouse button down

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const container = document.getElementById('container');
-console.log(container);
-
-let cellPerSide = 20;
+let trigger = false;
+let cellPerSide = 100;
 let cellWidth = 800/cellPerSide;
+
 container.style.gridTemplateColumns = `repeat(${cellPerSide}, 1fr)`;
+document.addEventListener('mousedown', function() {
+    trigger= true;
+});
+document.addEventListener('mouseup', () => {
+    trigger=false;
+});
 
 for (let i=0; i<(cellPerSide*cellPerSide); i++) {
     const newDiv = document.createElement('div');
+    newDiv.addEventListener('mouseenter', () => {
+        if (trigger === true) {
+            newDiv.classList.add('hover');
+        }
+    })
     newDiv.style.width=`${cellWidth}px`;
     newDiv.style.height=`${cellWidth}px`;
-    newDiv.style.border='0.5px solid #777';
+    newDiv.style.border='0.5px solid #999';
+    // newDiv.innerText=`${i}`;
     container.appendChild(newDiv);
 }
 
