b79841d677a8e10581cd587df2687cb3e61ad146
Chris <ckatanic@gmail.com>
2023-08-04 19:14:22 -0700

    add button to change number of squares per row

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const container = document.getElementById('container');
+const button = document.getElementById('button');
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    container.innerHTML="";
+    let numberPerGrid = document.getElementById('numberPerRow').value;
+    createGrid(numberPerGrid);
+})
 let trigger = false;
-let cellsPerSide = 52;
-let cellWidth = 600/cellsPerSide;
 
-container.style.gridTemplateColumns = `repeat(${cellsPerSide}, 1fr)`;
-container.style.gap='0';
-document.addEventListener('mousedown', function() {
-    trigger= true;
-});
-document.addEventListener('mouseup', () => {
-    trigger=false;
-});
+function createGrid(number) {
+    let cellsPerSide = number;
+    let cellWidth = 600/cellsPerSide;
 
-for (let i=1; i<(cellsPerSide*cellsPerSide+1); i++) {
-    const newDiv = document.createElement('div');
-    newDiv.addEventListener('mouseenter', () => {
-        if (trigger === true) {
-            newDiv.classList.add('hover');
-        }
-    })
-    newDiv.style.width=`${cellWidth}px`;
-    newDiv.style.height=`${cellWidth}px`;
-    newDiv.style.border='0.5px solid #999';
-    // newDiv.innerText=`${i}`;
-    container.appendChild(newDiv);
+    container.style.gridTemplateColumns = `repeat(${cellsPerSide}, 1fr)`;
+    container.style.gap='0';
+    document.addEventListener('mousedown', function() {
+        trigger= true;
+    });
+    document.addEventListener('mouseup', () => {
+        trigger=false;
+    });
+
+    for (let i=1; i<(cellsPerSide*cellsPerSide+1); i++) {
+        const newDiv = document.createElement('div');
+        newDiv.addEventListener('mouseenter', () => {
+            if (trigger === true) {
+                newDiv.classList.add('hover');
+            }
+        })
+        newDiv.style.width=`${cellWidth}px`;
+        newDiv.style.height=`${cellWidth}px`;
+        newDiv.style.border='0.5px solid #999';
+        // newDiv.innerText=`${i}`;
+        container.appendChild(newDiv);
+    }
 }
+
+createGrid(16);
 
 
