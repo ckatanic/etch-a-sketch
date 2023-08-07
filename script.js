4a82a3daf531b1f5b2e615863732ba5e7e37e06d
Chris <ckatanic@gmail.com>
2023-08-06 17:11:12 -0700

    remove comments from code

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ let mode = 'normal';
 let trigger = false;
 let currentGridSize = 16;
 
-console.log(document.getElementById(`${mode}`));
 
 const container = document.getElementById('grid-container');
 const button = document.getElementById('button');
 const sizeSlider = document.getElementById('gridSizeSlider');
-const gridSizeDisplay = document.getElementById('gridSizeDisplay');
+const selectedGridSize = document.getElementById('selectedGridSize');
 const normalButton = document.getElementById('normal');
 const rainbowButton = document.getElementById('rainbow');
 const eraserButton = document.getElementById('eraser');
 const shaderButton = document.getElementById('shader');
 const resetButton = document.getElementById('reset');
 
-
+document.addEventListener('mousedown', function() {
+    trigger= true;
+});
+document.addEventListener('mouseup', () => {
+    trigger=false;
+});
 normalButton.addEventListener('click', function() {
     removeActiveClassFromCurrentMode();
     mode='normal';
@@ -43,10 +47,10 @@ sizeSlider.onchange = (e) => {
     
     createGrid(e.target.value);
     currentGridSize = e.target.value;
-    gridSizeDisplay.innerText=`${e.target.value} x ${e.target.value}`;
+    selectedGridSize.innerText=`${e.target.value} x ${e.target.value}`;
 }
 sizeSlider.onmousemove = (e) => {
-    gridSizeDisplay.innerText=`${e.target.value} x ${e.target.value}`;
+    selectedGridSize.innerText=`${e.target.value} x ${e.target.value}`;
 }
 
 function removeActiveClassFromCurrentMode() {
@@ -61,12 +65,6 @@ function createGrid(number) {
 
     container.style.gridTemplateColumns = `repeat(${cellsPerSide}, 1fr)`;
     container.style.gap='0';
-    document.addEventListener('mousedown', function() {
-        trigger= true;
-    });
-    document.addEventListener('mouseup', () => {
-        trigger=false;
-    });
 
     for (let i=0; i<(cellsPerSide*cellsPerSide); i++) {
         const newDiv = document.createElement('div');
@@ -109,7 +107,6 @@ function createGrid(number) {
                             break;
                     }
                 } else if (mode === 'normal') {
-                    console.log('test')
                     cell.style.backgroundColor='rgba(0, 0, 0, 1)';
                 } else if (mode === 'rainbow') {
                     let r = (Math.floor(Math.random()*256));
